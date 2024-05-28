299865f2da1a85bb23ee80fd23305a70a2b560f6
ryo izuta <r.izuta@mplpl.com>
2024-05-28 16:08:18 +0900

    ✨ NGワードの入力規制の実装の途中

---
+++ src/Components/FormBasic.jsx
@@ -0,0 +1,84 @@
+import { useForm } from "react-hook-form";
+
+export default function FormBasic() {
+  const defaultValues = {
+    name: '出田怜',
+    email: 'admin@example.com',
+    gender: 'none',
+    memo: 'ここはメモです'
+  };
+
+  //フォームの初期化
+  const {register, handleSubmit, formState: {errors}} = useForm( {
+    defaultValues
+  });
+
+  //submit時の処理
+  const onsubmit = data => console.log(data);
+  const onerror = err => console.log(err);
+
+  return(
+    <form onSubmit={handleSubmit(onsubmit,onerror)} noValidate>
+      <div>
+        <label htmlFor="name">名前：</label><br />
+        <input id="name" type="text"
+          {...register('name',{
+            required: '名前は必須です',
+            maxLength: {
+              value: 20,
+              message: '名前は20文字以内にしてください'
+            }
+          })} />
+          <div>{errors.name?.message}</div>
+      </div>
+      <div>
+        <label htmlFor="gender">性別：</label><br />
+        <label>
+        <input type="radio" value="male"
+          {...register('gender',{
+            required: '性別は必須です'
+          })} />男性
+        </label>
+        <label>
+        <input type="radio" value="female"
+          {...register('gender',{
+            required: '性別は必須です'
+          })} />女性
+        </label>
+          <div>{errors.name?.message}</div>
+      </div>
+      <div>
+        <label htmlFor="email">メールアドレス</label><br/>
+        <input id="email" type="email"
+        {...register('email', {
+          required: 'メールアドレスは必須入力',
+          pattern: {
+            value: /([a-z\d+\-.]+)@([a-z\d-]+(?:\.[a-z]+)*)/i,
+            message: 'メールアドレスの形式が不正です'
+          }
+        })}/>
+        <div>{errors.email?.message}</div>
+      </div>
+      <div>
+        <label htmlFor="memo">備考：</label>
+        <textarea id="memo"
+          {...register('memo',{
+            required: '備考は必須入力です',
+            minLength: {
+              value: 10,
+              message:'備考は10文字以上にしてください'
+            },
+            validate: {
+              ng: (value, formValues) => {
+                const ngs = ['暴力', '死', 'グロ'];
+              }
+            }
+          })} />
+          <div>{errors.memo?.message}</div>
+      </div>
+      <div>
+        <button type="submit">送信</button>
+      </div>
+    </form>
+  );
+}
